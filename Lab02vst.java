01d4b2f9a3da3a3bc4ede565b4baff016f66ce23
Mr. Kiedes <akiedes@vtsd.com>
2022-09-14 07:49:43 -0400

    Code Update Upload

--- Lab02vst.java
+++ Lab02vst.java
@@ -1,1 +1,27 @@
-  
+/*
+    Welcome to your first lab!  All work should be completed inside of this file.
+    Please make sure that as you work on your lab you save, commit, and push everyday.
+
+    As always, if you have any questions reach out and email me!
+
+*/
+
+  public class Lab02vst
+  {
+
+    public static void main(String args[])
+    {
+      System.out.println("Lab 02, 100 Point Version") ;
+      System.out.println() ;
+      System.out.println() ;
+      System.out.print("Measure today's Knowledge ") ;
+      System.out.println("by yesterday's confusion. ") ;
+      System.out.println() ;
+      System.out.println("Bewilderment + Exposure + Obvious") ;
+      System.out.println() ;
+      System.out.println("# ##### #####    ##### ##### # #   # ##### #####" ) ;
+      System.out.println("# #   # #   #    #   # #   # # ##  #   #   #") ;
+      System.out.println("# #   #") ;
+
+    }
+  }
